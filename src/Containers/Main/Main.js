763e4c06b0afabbe733bddf06f0596ffc7967a67
DragonFyZex <brandonli456@hotmail.com>
2019-05-22 00:39:01 -0700

    Added Rbx.Bet not affiliated

--- src/Containers/Main/Main.js
+++ src/Containers/Main/Main.js
@@ -5,12 +5,15 @@ import Jackpot from '../Jackpot/Jackpot'
 
 export default () => {
     return (
-        <div className = "mainContainer">
-            <Jackpot />
-            <div className = "infoContainer">
+        <div style = {{display: 'flex', flexDirection: "column", flexGrow: 1}}>
+            <div className = "mainContainer">
+                <Jackpot />
+                <div className = "infoContainer">
 
+                </div>
+                
             </div>
-            
+            <p style = {{marginTop: "auto", fontFamily: "Fira Sans", color: "white"}}> RBX.BET is not affiliated with Roblox Corporation </p>
         </div>
     )
 }
