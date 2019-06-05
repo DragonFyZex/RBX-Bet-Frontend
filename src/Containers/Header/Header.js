35053c9275c56016e368ca5970f10c6ef133eb9e
DragonFyZex <brandonli456@hotmail.com>
2019-06-04 23:13:43 -0700

    Changed Support to Discord

--- src/Containers/Header/Header.js
+++ src/Containers/Header/Header.js
@@ -11,7 +11,7 @@ export default ({sidebarIsOpen, setSidebarIsOpen}) => (
             </span>
         </button> */}
         <Link to="/" className="screenText screenTextClickable" href="https://discord.gg/vx6MA7S" style={{marginLeft: 'auto'}}>Play</Link>
-        <a className="screenText screenTextClickable" href="https://discord.gg/vx6MA7S" target="_blank"  style={{marginLeft: '2%'}}>Support</a>
+        <a className="screenText screenTextClickable" href="https://discord.gg/vx6MA7S" target="_blank"  style={{marginLeft: '2%'}}>Discord</a>
         <a className="screenText screenTextClickable" href="https://twitter.com/rbxbet" target="_blank" style={{marginLeft: '2%'}}>Social</a>
         <Link to="/tos" className="screenText screenTextClickable" target="_blank"  style={{marginRight: '3%'}}>Terms Of Service</Link >
     </div>
