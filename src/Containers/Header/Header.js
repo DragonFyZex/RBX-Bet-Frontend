6c59930c5abc65c0cb5d750411d8656d5931dbd1
DragonFyZex <brandonli456@hotmail.com>
2019-06-14 14:26:38 -0700

    Changed discord link

--- src/Containers/Header/Header.js
+++ src/Containers/Header/Header.js
@@ -10,8 +10,8 @@ export default ({sidebarIsOpen, setSidebarIsOpen}) => (
                 <span className="hamburger-inner" />
             </span>
         </button> */}
-        <Link to="/" className="screenText screenTextClickable" href="https://discord.gg/vx6MA7S" style={{marginLeft: 'auto'}}>Play</Link>
-        <a className="screenText screenTextClickable" href="https://discord.gg/vx6MA7S" target="_blank"  style={{marginLeft: '2%'}}>Discord</a>
+        <Link to="/" className="screenText screenTextClickable" style={{marginLeft: 'auto'}}>Play</Link>
+        <a className="screenText screenTextClickable" href="https://discord.gg/BmPFzTT" target="_blank"  style={{marginLeft: '2%'}}>Discord</a>
         <a className="screenText screenTextClickable" href="https://twitter.com/rbxbet" target="_blank" style={{marginLeft: '2%'}}>Social</a>
         <Link to="/tos" className="screenText screenTextClickable" target="_blank"  style={{marginRight: '3%'}}>Terms Of Service</Link >
     </div>
